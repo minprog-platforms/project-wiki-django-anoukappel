5aa5e51521233b48e5f4d02138d61bee041d6cc1
anoukappel <anouk-appel@hotmail.com>
2022-11-10 16:13:40 +0100

    converted markdown to html

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -1,11 +1,12 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
-# app_name = "wiki"
+app_name = "wiki"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>/", views.wiki, name="title"),
-    path("search/", views.search, name="zoek"),
-    path("new_page/", views.new_page, name="new_page")
+    path("wiki/<str:title>", views.title, name="title"),
+    re_path(r"^wiki/(?P<title>).*[\s\w]*/$", views.wiki_entry, name="wiki_entry"),
+    path("search", views.search, name="zoek"),
+    path("add_file", views.add_file, name="add_file")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,59 +1,108 @@
 from django.shortcuts import render
 from . import util
 from django import forms
-# from markdown2 import Markdown
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from markdown2 import markdown
 
-# creating a form
-class InputForm(forms.Form):
-    title = forms.CharField(max_length = 200)
-    content = forms.CharField()
+class SaveNewPage(forms.Form):
+    title = forms.CharField(label="Title")
+    content = forms.CharField(label = "Content")
+
+
+# Add a new Page
+def add_file(request):
+    return render(request, "encyclopedia/add_file.html", {
+        "form": SaveNewPage()
+    })
+#
+# # add a new Page
+# def add_file(request):
+#     #check if method is POST
+#     if request.method == "POST":
+#
+#     # Take in the data the user submitted and save it as form
+#         title = SaveNewPage(request.POST)
+#
+#     # check if form data is valid server_side
+#     if form.is_valid():
+#
+#         #isolate the task from the 'cleaned' version of the form data
+#         file = title.cleaned_data["task"]
+#
+#         # Add the new task to out
+#         tasks.append(task)
+#
+#         # redirect user to list of tasks
+#         return HttpResponseRedirect(reverse("encyclopedia/add_file.html"))
+#     else:
+#         # If the form invalid, re-render the page with existing information.
+#         return render(request, "encyclopedia/add_file.html", {
+#                  "form": form
+#         })
+#          return render(request, "encyclopedia/add_file.html", {
+#         "form": SaveNewPage
+#     })
 
 
 def index(request):
     """
     Zorgt ervoor dat verschillende titles geprint kunnen worden op de hoofdpagina.
     """
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+    if request.GET.get('q'):
+        search(request, request.GET['q'])
+    else:
+        return render(request, "encyclopedia/index.html", {
+            "entries": util.list_entries()
     })
 
+def search(request, query):
+    """
+    z
+    """
+    match = []
+    return render(request, "encyclopedia/wiki.html", {
+        # "content": util.get_entry(zoek),
+        "query": query,
+        "result": match
+    })
 
-def wiki(request, title):
+def title(request, title):
     """
     zorgt ervoor dat de content en de title van de pagina geprint kan worden in wiki.html.
     """
+    tekst = util.get_entry(title)
+    if tekst:
+        """
+        als er tekst in de title.md file is convert markdown naar html en return
+        html tekst en title, in .html file html|safe om juiste manier te displayen.
+        """
+        html = markdown(util.get_entry(title)).strip()
+        return render(request, "encyclopedia/wiki.html", {
+            "content": html,
+            "title": title
+        })
+    else:
+        return render(request, "encyclopedia/notfound.html", {
+            "error": f"Error: Wiki page title '{title}' not found"
+        })
+
+def wiki_entry(request, title):
+    """uitleg"""
+    context = {}
+    entry_list = utily.list_entries()
+
+    #filter the title
+    title = title.strip()
+    wiki = [entry for entry in entry_list if title.lower() in entry.lower()]
+
+    if not wiki or wiki is None:
+        return redirect(notFound)
+
+    # get Entry by its title
+    entry = util.get_entry(wiki[0])
+    content = markdown(entry).strip()
     return render(request, "encyclopedia/wiki.html", {
-        "content": util.get_entry(title),
+        "content": content,
         "title": title
     })
-
-def search(request, zoek):
-    """
-    zorgt ervoor dat de content en de title van de pagina geprint kan worden in wiki.html.
-    """
-    return render(request, "encyclopedia/wiki.html", {
-        "content": util.get_entry(zoek),
-        "title": zoek
-    })
-
-# def search(request):
-#     return render(request, "encyclopedia/layout.html", {
-#
-#     })
-
-
-def new_page(request):
-    return render(request, "encyclopedia/new_page.html", {
-    "title": "Title",
-    "edit": False,
-    "editpage":InputForm()
-    })
-    # context ={}
-    # context['form'] = InputForm()
-    # return render(request, "encyclopedia/new_page.html", context)
-
-
-# def entry_page(request):
-#     return render(request, "encyclopedia/entry_page.html",{
-#         "title": util.get_entry(index)
-#     })
