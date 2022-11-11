7d0f591200b8c809b7a89ffe5d3ec629d1d49164
anoukappel <anouk-appel@hotmail.com>
2022-11-11 15:57:56 +0100

    alles werkt!

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,7 +5,10 @@ from . import views
 app_name = "wiki"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.entry, name="entry"),
-    path("search", views.search, name="search"),
-    path("add_file", views.add_file, name="add_file")
+    path("wiki/<str:title>/", views.entry, name="entry"),
+    path("search/", views.search, name="search"),
+    path("new_page/", views.new_page, name="new_page"),
+    path("random_page/", views.random_page, name="random_page"),
+    path("change_page/", views.change_page, name="change_page"),
+    path("save_edit_page/", views.save_edit_page, name="save_edit_page")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -4,14 +4,25 @@ from django import forms
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 from markdown2 import markdown
+from random import randint
 
-class SaveNewPage(forms.Form):
-    title = forms.CharField(label="Title")
-    content = forms.CharField(label = "Content")
+
+def index(request):
+    """
+    Zorgt ervoor dat verschillende titles geprint kunnen worden op de homepagina.
+    """
+    if request.GET.get('q'):
+        search(request, request.GET['q'])
+    else:
+        return render(request, "encyclopedia/index.html", {
+            "entries": util.list_entries()
+    })
 
 def convert_md_to_html(title):
     """
-    converts markdown file ot html.
+    converts markdown file ot html of given title.
+    use get_entry to get content of file.
+    If succes return true else false.
     """
     content = util.get_entry(title)
     if content == None:
@@ -20,32 +31,35 @@ def convert_md_to_html(title):
         html = markdown(content)
         return html
 
+
 def entry(request, title):
+    """
+    Geeft titel en content in html door naar wiki.html zodat dit weergegeven kan worden.
+    """
     html_content = convert_md_to_html(title)
     if html_content == None:
         return render(request, "encyclopedia/error.html", {
             "Error": "this entry is missing"
         })
     else:
-        return render(request, "encyclopedia/entry.html", {
-        "title": title,
-        "content": html_content
+        return render(request, "encyclopedia/wiki.html", {
+            "content": html_content,
+            "title": title
         })
 
 
 def search(request):
     """
-    check of er iets in de search balk is gezocht. ('q')
-        * als 'q' een entry is direct naar die pagina
-        * anders show entries waar 'q' in voorkomt. als 'q'=py show
-          list met python
+    check of er iets in de search balk is gezocht. ('zoekterm')
+        * als 'zoekterm' een entry is direct naar die pagina
+        * anders show entries waar 'zoekterm' in voorkomt. als 'zoekterm'=py show
+          list met python.
     """
     if request.method == "POST":
-        entry_search = request.POST['q']
+        entry_search = request.POST['zoekterm']
         html_content = convert_md_to_html(entry_search)
         if html_content is not None:
-            return render(request, "encyclopedia/entry.html", {
-                "title": entry_search,
+            return render(request, "encyclopedia/wiki.html", {
                 "content": html_content
             })
         else:
@@ -59,76 +73,65 @@ def search(request):
             })
 
 
-# Add a new Page
-def add_file(request):
-    return
-
-
-
-    # match = []
-    # return render(request, "encyclopedia/wiki.html", {
-    #     # "content": util.get_entry(zoek),
-    #     "query": query,
-    #     "result": match
-    #})
-
-
-def title(request, title):
+def new_page(request):
     """
-    zorgt ervoor dat de content en de title van de pagina geprint kan worden in wiki/title
+    wanneer op new_page gedrukt wordt (GET), naar pagina waar title en content ingevuld kan worden.
+    Wanneer POST method (op button), check of title al bestaat en render  error page
+    anders save as new file en render naar wiki.html met nieuwe titel en content.
     """
-    tekst = util.get_entry(title)
-    if tekst:
-        """
-        als er tekst in de title.md file is convert markdown naar html en return
-        html tekst en title, in .html file html|safe om juiste manier te displayen.
-        """
-        html = markdown(util.get_entry(title)).strip()
-        return render(request, "encyclopedia/wiki.html", {
-            "content": html,
-            "title": title
-        })
+    if request.method == "GET":
+        return render(request, "encyclopedia/new_page.html")
     else:
-        return render(request, "encyclopedia/notfound.html", {
-            "error": f"Error: Wiki page title '{title}' not found"
-        })
+        title = request.POST['title']
+        content = request.POST['Content']
+        if util.get_entry(title) is not None:
+            return render(request, "encyclopedia/error.html", {
+                "message": "Entry page already exists"
+            })
+        else:
+            util.save_entry(title, content)
+            html_content = convert_md_to_html(title)
+            return render(request, "encyclopedia/wiki.html", {
+                "title": title,
+                "content": html_content
+            })
 
-
-
-
-
-
-
-def index(request):
+def random_page(request):
     """
-    Zorgt ervoor dat verschillende titles geprint kunnen worden op de hoofdpagina.
+    render wiki.html van een random title.
     """
-    if request.GET.get('q'):
-        search(request, request.GET['q'])
-    else:
-        return render(request, "encyclopedia/index.html", {
-            "entries": util.list_entries()
-    })
-
-
-
-
-def wiki_entry(request, title):
-    """uitleg"""
-    context = {}
-    entry_list = utily.list_entries()
-
-    #filter the title
-    title = title.strip()
-    wiki = [entry for entry in entry_list if title.lower() in entry.lower()]
-
-    if not wiki or wiki is None:
-        return redirect(notFound)
-
-    # get Entry by its title
-    entry = util.get_entry(wiki[0])
-    content = markdown(entry).strip()
+    length_list = len(util.list_entries())
+    list = util.list_entries()
+    content = convert_md_to_html(list[randint(0, length_list - 1)])
+    title = list[randint(0, length_list - 1)]
     return render(request, "encyclopedia/wiki.html", {
         "content": content,
         "title": title
     })
+
+def change_page(request):
+    """
+    wanneer POST method (wiki.html) render change_page.
+    waar je title en content van pagina kunt aanpassen.
+    """
+    if request.method == "POST":
+        title = request.POST['entry_title']
+        content = util.get_entry(title)
+        return render(request, "encyclopedia/change_page.html", {
+            "content": content,
+            "title": title
+        })
+
+def save_edit_page(request):
+    """
+    sla aangepaste pagina op.
+    """
+    if request.method == "POST":
+        title = request.POST['title']
+        content = request.POST['Content']
+        util.save_entry(title, content)
+        html_content = convert_md_to_html(title)
+        return render(request, "encyclopedia/wiki.html", {
+            "title": title,
+            "content": html_content
+        })
